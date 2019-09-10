b0ae5ecdb22a517bd6ea8a45adb212e1bac68777
Louis Su <louis222220@gmail.com>
2019-09-10 15:12:53 +0800

    Fill the sum_n function

--- sum_n.php
+++ sum_n.php
@@ -1,10 +1,14 @@
 <?php
 // sum_n.php
 
-function sum_n(){
-    return 1;
+function sum_n($num){
+    $result = 0;
+    for($i = 1; $i<=$num; $i++){
+        $result += $i;
+    }
+    return $result;
 }
 
-assert(sum_n());
+assert(sum_n(4)==10);
 
 ?>
