3322bfa28dba4b110f9fd9bce7926f944d3e6874
Louis Su <louis222220@gmail.com>
2019-09-17 13:49:23 +0800

    Add unit test for Salary object

---
+++ Salary.php
@@ -0,0 +1,19 @@
+<?php
+// Salary.php
+
+class Salary{
+    
+}
+
+// ---tests---
+run_tests();
+
+function run_tests(){
+    test_set_salary();
+}
+
+function test_set_salary(){
+    $tmp_salary = new Salary();
+}
+
+?>
